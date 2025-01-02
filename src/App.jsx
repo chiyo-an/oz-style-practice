72ce0168729b31724567291b82ec072fffb0fca7
chiyo-an <chiyoawesome@gmail.com>
2025-01-02 16:52:50 +0900

    [React] 11일차 과제 - SCSS 파일을 Styled-Components로 리팩토링

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
-import "./App.scss";
-import { contents } from "./assets/data/data";
+//import "./App.scss";
+
 import Content from "./components/Content";
 import Header from "./components/Header";
+import { contents } from "./assets/data/data";
+import styled from 'styled-components';
+
+const MainSection = styled.section`
+  display: flex; 
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 20px;
+  padding: 20px 40px;
+`;
 
 function App() {
   return (
     <main>
       <Header />
-      <section>
+      <MainSection>
         {contents.map((el) => (
           <Content key={el.id} content={el} />
         ))}
-      </section>
+      </MainSection>
     </main>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,35 @@
+import styled from 'styled-components';
+
+const HeaderWrapper = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px 20px;
+  background-color: black;
+`;
+
+const MenuList = styled.ul`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+`;
+
+const MenuItem = styled.li`
+  font-size: 16px;
+  font-weight: 400;
+  list-style: none;
+`;
+
 export default function Header() {
   return (
-    <header className="header-container">
+    <HeaderWrapper>
       <h1>OZ코딩스쿨</h1>
-      <ul className="menu-container">
-        <li>로그인</li>
-        <li>회원가입</li>
-        <li>내클래스</li>
-      </ul>
-    </header>
+      <MenuList>
+        <MenuItem>로그인</MenuItem>
+        <MenuItem>회원가입</MenuItem>
+        <MenuItem>내클래스</MenuItem>
+      </MenuList>
+    </HeaderWrapper>
   );
 }
